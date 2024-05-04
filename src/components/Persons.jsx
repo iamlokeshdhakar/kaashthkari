39d6956d84ae9e298e3b070af465ffc6b5573b7e
lokeshdhakar97 <lokeshdhakar1404@gmail.com>
2024-05-04 18:14:39 +0530

    add person section

--- src/components/Persons.jsx
+++ src/components/Persons.jsx
@@ -1,7 +1,19 @@
+import Image from 'next/image'
 import '../styles/persons.css'
 const Persons = () => {
   return (
-    <div className="persons">Persons</div>
+    <div className="persons">
+      <div className='p-arrow'>
+        <Image src={'/arrow.png'} width={80} height={50}/>
+      </div>
+      <div className='p-img'>
+        <Image src={'/person.png'} width={900} height={250}/>
+        <Image src={'/person.png'} width={900} height={250}/>
+      </div>
+      <div className='p-text'>
+        <h1>KNOW THE CONNOISSEURS</h1>
+      </div>
+    </div>
   )
 }
 
