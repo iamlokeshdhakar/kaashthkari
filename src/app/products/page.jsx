d90a951cb17672c36f3a6f78eeaac2099d737c82
lokeshdhakar97 <lokeshdhakar1404@gmail.com>
2024-05-06 19:24:31 +0530

    add product slider in product page

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,6 +1,19 @@
+"use client"
 import "@/styles/products.css"
+import { useRef } from "react"
 
 const Produts = () => {
+  const scrollRef = useRef(null)
+
+  const scrollRight = () => {
+    window.scroll({
+      behavior: "smooth",
+    })
+    if (scrollRef.current) {
+      scrollRef.current.scrollLeft += 930
+    }
+  }
+
   return (
     <div className="products">
       <div className="b-img"></div>
@@ -8,13 +21,45 @@ const Produts = () => {
         <div className="box">
           <h2>Buyer&apos;s Choice</h2>
           <div className="wrapper">
-            <div className="cards">
-              <div className="card"></div>
-              <div className="card"></div>
-              <div className="card"></div>
-              <div className="card"></div>
+            <div className="card-box" ref={scrollRef}>
+              <div className="cards">
+                <div className="card">
+                  <img src="/product-img/bc1.png" alt="" />
+                </div>
+                <div className="card">
+                  <img src="/product-img/bc2.png" alt="" />
+                </div>
+                <div className="card">
+                  <img src="/product-img/bc3.png" alt="" />
+                </div>
+              </div>
+
+              <div className="cards">
+                <div className="card">
+                  <img src="/product-img/bc1.png" alt="" />
+                </div>
+                <div className="card">
+                  <img src="/product-img/bc2.png" alt="" />
+                </div>
+                <div className="card">
+                  <img src="/product-img/bc3.png" alt="" />
+                </div>
+              </div>
+
+              <div className="cards">
+                <div className="card">
+                  <img src="/product-img/bc1.png" alt="" />
+                </div>
+                <div className="card">
+                  <img src="/product-img/bc2.png" alt="" />
+                </div>
+                <div className="card">
+                  <img src="/product-img/bc3.png" alt="" />
+                </div>
+              </div>
             </div>
-            <div className="arrow">
+
+            <div onClick={scrollRight} className="arrow">
               <svg
                 width="42"
                 height="42"
@@ -38,7 +83,6 @@ const Produts = () => {
           </div>
         </div>
       </div>
-      Produts
     </div>
   )
 }
