674936e31db2767af4bf9a6630b392e6b4c0c9ce
lokeshdhakar97 <lokeshdhakar1404@gmail.com>
2024-05-06 17:19:14 +0530

    add banaras page

---
+++ src/app/products/page.jsx
@@ -0,0 +1,46 @@
+import "@/styles/products.css"
+
+const Produts = () => {
+  return (
+    <div className="products">
+      <div className="b-img"></div>
+      <div className="items">
+        <div className="box">
+          <h2>Buyer's Choice</h2>
+          <div className="wrapper">
+            <div className="cards">
+              <div className="card"></div>
+              <div className="card"></div>
+              <div className="card"></div>
+              <div className="card"></div>
+            </div>
+            <div className="arrow">
+              <svg
+                width="42"
+                height="42"
+                viewBox="0 0 47 99"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M2.4252 1.00043L44.4209 48.8264L1.9999 97.9636"
+                  stroke="#5C2811"
+                  stroke-width="3"
+                />
+                <path
+                  d="M2.4252 1.00043L44.4209 48.8264L1.9999 97.9636"
+                  stroke="#5C2811"
+                  stroke-opacity="0.2"
+                  stroke-width="3"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+      Produts
+    </div>
+  )
+}
+
+export default Produts
