ddf56ba2f0e8bd3a1dbe0d0e7a8944118667adb9
lokeshdhakar97 <lokeshdhakar1404@gmail.com>
2024-05-11 13:24:01 +0530

    product page responsive and mde changes

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -3,11 +3,6 @@ import "@/styles/products.css"
 import { useRef } from "react"
 
 const Produts = () => {
-  const scrollRef1 = useRef(null)
-  const scrollRef2 = useRef(null)
-  const scrollRef3 = useRef(null)
-  const scrollRef4 = useRef(null)
-
   const productJson = [
     {
       heading: `Buyer's Choice`,
@@ -15,8 +10,14 @@ const Produts = () => {
         "/product-img/bc1.png",
         "/product-img/bc2.png",
         "/product-img/bc3.png",
+        "/product-img/bc1.png",
+        "/product-img/bc2.png",
+        "/product-img/bc3.png",
+        "/product-img/bc3.png",
+        "/product-img/bc1.png",
+        "/product-img/bc2.png",
+        "/product-img/bc3.png",
       ],
-      ref: scrollRef1,
     },
     {
       heading: "Decorative",
@@ -24,8 +25,13 @@ const Produts = () => {
         "/product-img/dc1.png",
         "/product-img/dc2.png",
         "/product-img/dc3.png",
+        "/product-img/dc1.png",
+        "/product-img/dc2.png",
+        "/product-img/dc3.png",
+        "/product-img/dc1.png",
+        "/product-img/dc2.png",
+        "/product-img/dc3.png",
       ],
-      ref: scrollRef2,
     },
     {
       heading: "Mythological",
@@ -33,8 +39,13 @@ const Produts = () => {
         "/product-img/m1.png",
         "/product-img/m2.png",
         "/product-img/m3.png",
+        "/product-img/m1.png",
+        "/product-img/m2.png",
+        "/product-img/m3.png",
+        "/product-img/m1.png",
+        "/product-img/m2.png",
+        "/product-img/m3.png",
       ],
-      ref: scrollRef3,
     },
     {
       heading: "Utility",
@@ -42,30 +53,28 @@ const Produts = () => {
         "/product-img/u1.png",
         "/product-img/u2.png",
         "/product-img/u3.png",
+        "/product-img/u1.png",
+        "/product-img/u2.png",
+        "/product-img/u3.png",
+        "/product-img/u1.png",
+        "/product-img/u2.png",
+        "/product-img/u3.png",
       ],
-      ref: scrollRef4,
     },
   ]
 
-  const scrollRight = (ref) => {
-    window.scroll({
-      behavior: "smooth",
-    })
-    if (ref.current) {
-      ref.current.scrollLeft += 930
-    }
-  }
-
   return (
     <div className="products">
-      <div className="b-img"></div>
-      <div className="items">
+      <div className="b-img">
+        <img src="/banaras-img.png" alt="" />
+      </div>
+      {/* <div className="items">
         {productJson.map((e, k) => {
           return (
             <div className="box" key={k}>
               <h2>{e.heading}</h2>
               <div className="wrapper">
-                <div className="card-box" ref={e.ref}>
+                <div className="card-box">
                   {e.image.map((img, kk) => {
                     return (
                       <>
@@ -84,33 +93,27 @@ const Produts = () => {
                     )
                   })}
                 </div>
-
-                <div onClick={() => scrollRight(e.ref)} className="arrow">
-                  <svg
-                    width="42"
-                    height="42"
-                    viewBox="0 0 47 99"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M2.4252 1.00043L44.4209 48.8264L1.9999 97.9636"
-                      stroke="#5C2811"
-                      stroke-width="3"
-                    />
-                    <path
-                      d="M2.4252 1.00043L44.4209 48.8264L1.9999 97.9636"
-                      stroke="#5C2811"
-                      stroke-opacity="0.2"
-                      stroke-width="3"
-                    />
-                  </svg>
-                </div>
               </div>
             </div>
           )
         })}
-      </div>
+      </div> */}
+      {productJson.map((item) => {
+        return (
+          <div className="it-box">
+            <h2>{item.heading}</h2>
+            <div className="card-img-box">
+              {item.image.map((img) => {
+                return (
+                  <div className="card-img">
+                    <img src={img} alt="" />
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        )
+      })}
     </div>
   )
 }
